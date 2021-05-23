4ef0a9b25907ac7c942f7097cda5a5290c2be7ac
Thashini <61898159+thashini97@users.noreply.github.com>
2021-05-23 21:32:07 +0530

    Update scripts.js

--- Ex 05 - Challenge 1/scripts.js
+++ Ex 05 - Challenge 1/scripts.js
@@ -3,29 +3,29 @@
 // --------------------------------
 
 /*
-Kasun and Chamal are trying to compare their BMI (Body Mass Index), which is calculated using the formula: 
+student1 and student2 are trying to compare their BMI (Body Mass Index), which is calculated using the formula: 
 BMI = mass / height ^ 2
 (Mass is kg and Height in meter)
 
-1. Store Kasun's and Chamal's mass and height in variables
+1. Store student1's and student2's mass and height in variables
 2. Calculate both their BMIs
 3. Create a boolean variable containing information about whether Kasun has a higher BMI than Chamal.
 4. Print a string to in alert box containing the variable from step 3.
 */
 
-var mass, height, bmik, bmic, isKasun;
+var mass, height, bmik, bmic, isStudent1;
 
-// Kasun's BMI
-mass = prompt("Kasun's mass : ");
-height = prompt("Kasun's height : ");
+// student1's BMI
+mass = prompt("student1's mass : ");
+height = prompt("student1's height : ");
 bmik = mass / (height * height);
 
-// Chamal's BMI
-mass = prompt("Chamal's mass : ");
-height = prompt("Chamal's height : ");
+// student2's BMI
+mass = prompt("student2's mass : ");
+height = prompt("student2's height : ");
 bmic = mass / (height * height);
 
 // Boolean
-isKasun = (bmik > bmic);
+isStudent1 = (bmik > bmic);
 
-alert(isKasun);
+alert(student1);
